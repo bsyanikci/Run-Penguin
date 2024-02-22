96afbccb21aa57a00c2ed4acbce5f3128b8135e0
Serdar Yanıkçı <b.serdar.yanikci@gmail.com>
2024-02-22 20:21:05 +0300

    Müzik bugu düzeltildi

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
@@ -24,16 +25,22 @@ public class AudioManager : MonoBehaviour
     #endregion
     public List<Audio> audios = new List<Audio>();
     public AudioSource audioSource;
-
+    public Toggle audioToggle;
     bool isplaying = true;
 
     private void Awake()
     {
         //DontDestroyOnLoad(gameObject);
+        if (PlayerPrefs.GetInt("AudioEnabled", 1) == 0) 
+        {
+            audioToggle.isOn = false;
+            SwitchSoundState(false);
+        }
     }
     #region MUSÝC
     public void PlayMusic(string name)
     {
+        
         foreach (Audio audio in audios)
         {
             if (audio.soundName == name)
@@ -54,14 +61,17 @@ public class AudioManager : MonoBehaviour
     {
         if (isOn) 
         {
+            PlayerPrefs.SetInt("AudioEnabled", 1);
             audioSource.Play();
         }
         else
+
         {
+            PlayerPrefs.SetInt("AudioEnabled", 0);
             audioSource.Pause();
         }
 
-
+        PlayerPrefs.Save();
     }
     #endregion
    
